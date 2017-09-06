2874322fecb4243579beae1b019f8a8ea3576d74
Miha_x64 <mihlo1996@rambler.ru>
2017-09-06 14:40:46 +0300

    booleanOperators use common mapWith function

--- properties/src/main/kotlin/net/aquadc/properties/booleanOperators.kt
+++ properties/src/main/kotlin/net/aquadc/properties/booleanOperators.kt
@@ -1,16 +1,14 @@
 @file:Suppress("NOTHING_TO_INLINE")
 package net.aquadc.properties
 
-import net.aquadc.properties.internal.ConcurrentBiMappedCachedProperty
-
 inline fun Property<Boolean>.not() =
         map { !it }
 
 inline infix fun Property<Boolean>.and(that: Property<Boolean>): Property<Boolean> =
-        ConcurrentBiMappedCachedProperty(this, that, { a, b -> a && b })
+        mapWith(that) { a, b -> a && b }
 
 inline infix fun Property<Boolean>.or(that: Property<Boolean>): Property<Boolean> =
-        ConcurrentBiMappedCachedProperty(this, that, { a, b -> a || b })
+        mapWith(that) { a, b -> a || b }
 
 inline infix fun Property<Boolean>.xor(that: Property<Boolean>): Property<Boolean> =
-        ConcurrentBiMappedCachedProperty(this, that, { a, b -> a xor b })
+        mapWith(that) { a, b -> a xor b }
