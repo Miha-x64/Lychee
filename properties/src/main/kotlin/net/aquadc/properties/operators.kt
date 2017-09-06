df0def18835403d83083cc97a882f05b17a83521
Miha_x64 <mihlo1996@rambler.ru>
2017-09-06 14:47:36 +0300

    moved property delegation functions from operators to propertyDelegates

--- properties/src/main/kotlin/net/aquadc/properties/operators.kt
+++ properties/src/main/kotlin/net/aquadc/properties/operators.kt
@@ -1,19 +1,4 @@
 @file:Suppress("NOTHING_TO_INLINE")
 package net.aquadc.properties
 
-import net.aquadc.properties.internal.ConcurrentBiMappedCachedProperty
-import kotlin.reflect.KProperty
-
 inline fun <T> Property<T>.readOnlyView() = map { it }
-
-inline fun <A, B, T> Property<A>.mapWith(that: Property<B>, noinline transform: (A, B) -> T): Property<T> =
-        ConcurrentBiMappedCachedProperty(this, that, transform)
-
-
-inline operator fun <T> Property<T>.getValue(thisRef: Any?, property: KProperty<*>): T {
-    return value
-}
-
-inline operator fun <T> MutableProperty<T>.setValue(thisRef: Any?, property: KProperty<*>, value: T) {
-    this.value = value
-}
