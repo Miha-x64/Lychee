10b539a94d4255d45376d6908c4e777e4158a482
Miha_x64 <mihlo1996@rambler.ru>
2017-09-06 15:40:46 +0300

    CharSequence extensions

---
+++ properties/src/main/kotlin/net/aquadc/properties/charSequenceExtensions.kt
@@ -0,0 +1,12 @@
+package net.aquadc.properties
+
+
+inline val Property<CharSequence>.length get() = map { it.length }
+
+inline val Property<CharSequence>.isEmpty get() = map { it.isEmpty() }
+inline val Property<CharSequence>.isNotEmpty get() = map { it.isNotEmpty() }
+
+inline val Property<CharSequence>.isBlank get() = map { it.isBlank() }
+inline val Property<CharSequence>.isNotBlank get() = map { it.isNotBlank() }
+
+inline val Property<CharSequence>.trimmed get() = map { it.isNotBlank() }
