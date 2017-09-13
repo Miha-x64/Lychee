7f7fbbf7bbcd60908ad46cbfcfdc98991946bdd4
Miha_x64 <mihlo1996@rambler.ru>
2017-09-13 16:20:00 +0300

    'in' variance for consumer property

--- fx-bindings/src/main/kotlin/net/aquadc/properties/fx/bindTo.kt
+++ fx-bindings/src/main/kotlin/net/aquadc/properties/fx/bindTo.kt
@@ -3,7 +3,7 @@ package net.aquadc.properties.fx
 import net.aquadc.properties.Property
 import javafx.beans.property.Property as FxProperty
 
-fun <T> FxProperty<T>.bindTo(that: Property<T>) {
+fun <T> FxProperty<in T>.bindTo(that: Property<T>) {
     this.value = that.value
     that.addChangeListener { _, new ->
         this.value = new
