ff425c90cd40c0f392b39ea7dc49cfdbeea3ed40
Miha_x64 <mihlo1996@rambler.ru>
2018-02-07 18:45:45 +0300

    Added LeakCanary and dexcount, moved Android bindings to according package; made bindings unsubscribe automatically (#7)

---
+++ android-bindings/src/main/kotlin/net/aquadc/properties/android/bindings/TextView.kt
@@ -0,0 +1,90 @@
+package net.aquadc.properties.android.bindings
+
+import android.graphics.drawable.Drawable
+import android.text.Editable
+import android.text.SpannedString
+import android.widget.TextView
+import net.aquadc.properties.MutableProperty
+import net.aquadc.properties.Property
+import net.aquadc.properties.android.compat.getDrawableCompat
+import net.aquadc.properties.android.container.MessageAndIconRes
+import net.aquadc.properties.android.extension.getTextOrNullIfZero
+import net.aquadc.properties.android.extension.setErrorWithIntrinsicBounds
+import net.aquadc.properties.android.simple.SimpleTextWatcher
+
+
+fun TextView.bindTextTo(textProperty: Property<CharSequence>) =
+        bindViewTo(textProperty, ::setText)
+
+@JvmName("bindTextResTo")
+fun TextView.bindTextTo(textResProperty: Property<Int>) =
+        bindViewTo(textResProperty, ::setText)
+
+fun TextView.bindToText(textProperty: MutableProperty<CharSequence>) {
+    textProperty.value = text.toString()
+    addTextChangedListener(object : SimpleTextWatcher() {
+        override fun afterTextChanged(s: Editable) {
+            textProperty.value = SpannedString(s)
+        }
+    })
+}
+
+fun TextView.bindTextBidirectionally(textProperty: MutableProperty<String>) {
+    var mutatingFromWatcher = false
+    var mutatingFromChangeListener = false
+    addTextChangedListener(object : SimpleTextWatcher() {
+        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+            mutatingFromWatcher = true
+        }
+        override fun afterTextChanged(s: Editable) {
+            if (!mutatingFromChangeListener) textProperty.value = s.toString()
+            mutatingFromWatcher = false
+        }
+    })
+    bindViewTo(textProperty) { new ->
+        mutatingFromChangeListener = true
+        if (!mutatingFromWatcher) text = new
+        mutatingFromChangeListener = false
+    }
+}
+
+
+fun TextView.bindHintTo(hintProperty: Property<CharSequence>) =
+        bindViewTo(hintProperty, ::setHint)
+
+@JvmName("bindHintResTo")
+fun TextView.bindHintTo(hintResProperty: Property<Int>) =
+        bindViewTo(hintResProperty, ::setHint)
+
+
+fun TextView.bindErrorMessageTo(errorProperty: Property<CharSequence?>) =
+        bindViewTo(errorProperty, ::setError)
+
+@JvmName("bindErrorMessageResTo")
+fun TextView.bindErrorMessageTo(errorResProperty: Property<Int>) =
+        bindViewTo(errorResProperty) { new -> error = context.resources.getTextOrNullIfZero(new) }
+
+fun TextView.bindErrorMessageAndIconTo(errorProperty: Property<Pair<CharSequence, Drawable>?>) =
+        bindViewTo(errorProperty) { new ->
+            if (new == null) {
+                setError(null, null)
+            } else {
+                val (text, icon) = new
+                setErrorWithIntrinsicBounds(text, icon)
+            }
+        }
+
+@JvmName("bindErrorMessageResAndIconTo")
+fun TextView.bindErrorMessageAndIconTo(errorResProperty: Property<MessageAndIconRes?>) =
+        bindViewTo(errorResProperty) { new ->
+            resources.let { res ->
+                if (new == null) {
+                    setError(null, null)
+                } else {
+                    setErrorWithIntrinsicBounds(res.getText(new.messageRes), res.getDrawableCompat(new.iconRes))
+                }
+            }
+        }
+
+
+// todo: compound drawables
--- android-sample/src/main/kotlin/net/aquadc/properties/android/sample/App.kt
+++ android-sample/src/main/kotlin/net/aquadc/properties/android/sample/App.kt
@@ -1,6 +1,7 @@
 package net.aquadc.properties.android.sample
 
 import android.app.Application
+import com.squareup.leakcanary.LeakCanary
 import net.aquadc.properties.MutableProperty
 import net.aquadc.properties.concurrentMutablePropertyOf
 import net.aquadc.properties.sample.logic.InMemoryUser
@@ -9,5 +10,14 @@ import net.aquadc.properties.sample.logic.defaultUser
 class App : Application() {
 
     val userProp: MutableProperty<InMemoryUser> = concurrentMutablePropertyOf(defaultUser)
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this))
+            return
+
+        LeakCanary.install(this)
+    }
 
 }
