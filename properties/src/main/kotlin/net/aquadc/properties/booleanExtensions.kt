c24ec8b5a2796479570bd40c7d6f9ae8db5d73f3
Miha_x64 <mihlo1996@rambler.ru>
2018-02-07 19:09:44 +0300

    Added Property<Boolean> set() and reset() extensions, #6

--- properties/src/main/kotlin/net/aquadc/properties/booleanExtensions.kt
+++ properties/src/main/kotlin/net/aquadc/properties/booleanExtensions.kt
@@ -1,14 +1,33 @@
 @file:Suppress("NOTHING_TO_INLINE")
 package net.aquadc.properties
 
+
 inline operator fun Property<Boolean>.not() =
         map { !it }
 
 inline infix fun Property<Boolean>.and(that: Property<Boolean>): Property<Boolean> =
-        mapWith(that) { a, b -> a && b }
+        mapWith(that, And)
 
 inline infix fun Property<Boolean>.or(that: Property<Boolean>): Property<Boolean> =
-        mapWith(that) { a, b -> a || b }
+        mapWith(that, Or)
 
 inline infix fun Property<Boolean>.xor(that: Property<Boolean>): Property<Boolean> =
-        mapWith(that) { a, b -> a xor b }
+        mapWith(that, Xor)
+
+
+inline fun MutableProperty<Boolean>.set() { value = true }
+
+inline fun MutableProperty<Boolean>.reset() { value = false }
+
+
+// if we'd just use lambdas, they'd be copied into call-site
+
+@PublishedApi internal object And : (Boolean, Boolean) -> Boolean {
+    override fun invoke(p1: Boolean, p2: Boolean): Boolean = p1 && p2
+}
+@PublishedApi internal object Or : (Boolean, Boolean) -> Boolean {
+    override fun invoke(p1: Boolean, p2: Boolean): Boolean = p1 || p2
+}
+@PublishedApi internal object Xor : (Boolean, Boolean) -> Boolean {
+    override fun invoke(p1: Boolean, p2: Boolean): Boolean = p1 xor p2
+}
