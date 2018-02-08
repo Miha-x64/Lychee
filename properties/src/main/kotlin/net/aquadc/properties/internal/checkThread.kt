44f2ee502a58c26d11e95c10527292ab06eb20ad
Miha_x64 <mihlo1996@rambler.ru>
2018-02-08 23:15:01 +0300

    Better checkThread() error message

--- properties/src/main/kotlin/net/aquadc/properties/internal/checkThread.kt
+++ properties/src/main/kotlin/net/aquadc/properties/internal/checkThread.kt
@@ -1,6 +1,7 @@
 package net.aquadc.properties.internal
 
+
 internal fun checkThread(expected: Thread) {
     if (Thread.currentThread() !== expected)
-        throw RuntimeException("Only $expected allowed to touch this property.")
+        throw RuntimeException("${Thread.currentThread()} is not allowed to touch this property since it was created in $expected.")
 }
