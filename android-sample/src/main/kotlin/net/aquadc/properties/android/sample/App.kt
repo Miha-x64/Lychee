6d76a8df7a17a6ddc8626434ef63c80f7ace39a5
Miha_x64 <mihlo1996@rambler.ru>
2018-02-12 00:46:29 +0300

    added SharedPreferenceProperty, resolves #9

--- android-sample/src/main/kotlin/net/aquadc/properties/android/sample/App.kt
+++ android-sample/src/main/kotlin/net/aquadc/properties/android/sample/App.kt
@@ -1,15 +1,19 @@
 package net.aquadc.properties.android.sample
 
 import android.app.Application
+import android.content.SharedPreferences
+import android.preference.PreferenceManager
 import com.squareup.leakcanary.LeakCanary
 import net.aquadc.properties.MutableProperty
-import net.aquadc.properties.concurrentMutablePropertyOf
+import net.aquadc.properties.android.pref.PrefAdapter
+import net.aquadc.properties.android.pref.SharedPreferenceProperty
 import net.aquadc.properties.sample.logic.InMemoryUser
 import net.aquadc.properties.sample.logic.defaultUser
 
+
 class App : Application() {
 
-    val userProp: MutableProperty<InMemoryUser> = concurrentMutablePropertyOf(defaultUser)
+    lateinit var userProp: MutableProperty<InMemoryUser>
 
     override fun onCreate() {
         super.onCreate()
@@ -18,6 +22,28 @@ class App : Application() {
             return
 
         LeakCanary.install(this)
+
+        userProp = SharedPreferenceProperty(
+                PreferenceManager.getDefaultSharedPreferences(this), "user", defaultUser,
+                object : PrefAdapter<InMemoryUser> {
+                    override fun read(prefs: SharedPreferences, key: String, default: InMemoryUser) =
+                            InMemoryUser(
+                                    email = prefs.getString("${key}_email", default.email),
+                                    name = prefs.getString("${key}_name", default.name),
+                                    surname = prefs.getString("${key}_surname", default.surname)
+                            )
+                    override fun save(editor: SharedPreferences.Editor, key: String, value: InMemoryUser) {
+                        editor
+                                .putString("${key}_email", value.email)
+                                .putString("${key}_name", value.name)
+                                .putString("${key}_surname", value.surname)
+                    }
+                    override fun isKeyFor(propKey: String, prefKey: String): Boolean =
+                            prefKey == "${propKey}_email" ||
+                                    prefKey == "${propKey}_name" ||
+                                    prefKey == "${propKey}_surname"
+                }
+        )
     }
 
 }
