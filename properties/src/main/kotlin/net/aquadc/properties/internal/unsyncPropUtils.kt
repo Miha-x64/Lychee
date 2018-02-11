6b4add8deffa0d462d011212d09c6996d340588c
Miha_x64 <mihlo1996@rambler.ru>
2018-02-11 17:48:14 +0300

    more lightweight unsynchronized properties, resolves #10

---
+++ properties/src/main/kotlin/net/aquadc/properties/internal/unsyncPropUtils.kt
@@ -0,0 +1,44 @@
+package net.aquadc.properties.internal
+
+import net.aquadc.properties.ChangeListener
+
+
+internal fun checkThread(expected: Thread) {
+    if (Thread.currentThread() !== expected)
+        throw RuntimeException("${Thread.currentThread()} is not allowed to touch this property since it was created in $expected.")
+}
+
+/**
+ * Notifies change listener(s).
+ * If `null` passed, does nothing;
+ * if [ChangeListener] passed, notifies it;
+ * if [ArrayList] passed, notifies each listener;
+ * throws an [AssertionError] otherwise.
+ */
+@Suppress("UNCHECKED_CAST")
+internal fun <T> Any?.notifyAll(old: T, new: T) = when (this) {
+    null -> { /* no listeners, nothing to do */ }
+    is Function2<*, *, *> -> { (this as ChangeListener<T>)(old, new) }
+    is ArrayList<*> -> { (this as ArrayList<ChangeListener<T>>).forEach { it(old, new) } }
+    else -> throw AssertionError()
+}
+
+@Suppress("UNCHECKED_CAST")
+internal fun <T> Any?.plus(listener: ChangeListener<T>): Any = when (this) {
+    null -> listener
+    is Function2<*, *, *> -> ArrayList<ChangeListener<T>>(2).also { it.add(this as ChangeListener<T>); it.add(listener) }
+    isArrayList() -> (this as ArrayList<ChangeListener<T>>).apply { add(listener) }
+    else -> throw AssertionError()
+}
+
+@Suppress("UNCHECKED_CAST")
+internal fun <T> Any?.minus(listener: ChangeListener<T>): Any? = when (this) {
+    null -> null
+    listener -> null
+    is Function2<*, *, *> -> this
+    isArrayList() -> (this as ArrayList<ChangeListener<T>>).apply { remove(listener) }
+    else -> throw AssertionError()
+}
+
+@Suppress("NOTHING_TO_INLINE")
+private inline fun Any.isArrayList() = javaClass === ArrayList::class.java
