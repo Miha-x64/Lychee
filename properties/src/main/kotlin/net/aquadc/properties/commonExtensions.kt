847d9e3781e65193cdb9283c334ddecc0f0a7149
Miha_x64 <mihlo1996@rambler.ru>
2018-02-12 23:13:17 +0300

    added onEach extension

--- properties/src/main/kotlin/net/aquadc/properties/commonExtensions.kt
+++ properties/src/main/kotlin/net/aquadc/properties/commonExtensions.kt
@@ -19,3 +19,18 @@ object Identity : (Any?, Any?) -> Boolean {
 object Equals : (Any?, Any?) -> Boolean {
     override fun invoke(p1: Any?, p2: Any?): Boolean = p1 == p2
 }
+
+/**
+ * Calls [func] for each [Property.value] including initial.
+ */
+fun <T> Property<T>.onEach(func: (T) -> Unit) {
+    addChangeListener { _, new -> func(new) }
+    // *
+    func(value)
+
+    /*
+    What's better — call func first or add listener first?
+    a) if we call function first, in (*) place value may change and we won't see new one
+    b) if we add listener first, in (*) place value may change and we'll call func after that, in wrong order
+     */
+}
