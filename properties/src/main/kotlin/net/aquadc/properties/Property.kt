e329b2ef9747daba6a27fd0769bcc491d3bcca91
Miha_x64 <mihlo1996@rambler.ru>
2018-03-25 23:24:27 +0300

    added docs for Property interface

--- properties/src/main/kotlin/net/aquadc/properties/Property.kt
+++ properties/src/main/kotlin/net/aquadc/properties/Property.kt
@@ -1,12 +1,35 @@
 package net.aquadc.properties
 
+/**
+ * Represents an observable value.
+ */
 interface Property<out T> {
 
+    /**
+     * Returns current value.
+     */
     fun getValue(): T
+
+    /**
+     * When `true`, means that property value may change in future.
+     */
     val mayChange: Boolean
+
+    /**
+     * When `true`, means that property value can be
+     * mutated and observed from different threads.
+     */
     val isConcurrent: Boolean
 
+    /**
+     * Subscribe on value changes.
+     */
     fun addChangeListener(onChange: ChangeListener<T>)
+
+    /**
+     * Unsubscribe from value changes.
+     * It [onChange] is not registered as a listener, does nothing.
+     */
     fun removeChangeListener(onChange: ChangeListener<T>)
 
 }
