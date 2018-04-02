3e9fe0be6d1d05dac4141ff8c51e07763f90cd64
Mike <silizium@ok.de>
2018-04-02 12:21:32 +0300

    Added KDocs to booleanExtensions

--- properties/src/main/kotlin/net/aquadc/properties/booleanExtensions.kt
+++ properties/src/main/kotlin/net/aquadc/properties/booleanExtensions.kt
@@ -1,22 +1,39 @@
 @file:Suppress("NOTHING_TO_INLINE")
 package net.aquadc.properties
 
-
+/**
+ * Returns inverted view on this property.
+ */
 inline operator fun Property<Boolean>.not() =
         map(Not)
 
+/**
+ * Returns a view on [this] && [that].
+ */
 inline infix fun Property<Boolean>.and(that: Property<Boolean>): Property<Boolean> =
         mapWith(that, And)
 
+/**
+ * Returns a view on [this] || [that].
+ */
 inline infix fun Property<Boolean>.or(that: Property<Boolean>): Property<Boolean> =
         mapWith(that, Or)
 
+/**
+ * Returns a view on [this] ^ [that].
+ */
 inline infix fun Property<Boolean>.xor(that: Property<Boolean>): Property<Boolean> =
         mapWith(that, Xor)
 
-
+/**
+ * Sets [this] value to `true`.
+ */
 inline fun MutableProperty<Boolean>.set() { value = true }
 
+
+/**
+ * Sets [this] value to `false`.
+ */
 inline fun MutableProperty<Boolean>.clear() { value = false }
 
 /**
