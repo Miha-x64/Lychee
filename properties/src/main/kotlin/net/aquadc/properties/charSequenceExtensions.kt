e5b1c769b53f5c6dc8bc65163f2fa693621bd3d4
Miha_x64 <mihlo1996@rambler.ru>
2018-04-05 22:10:09 +0300

    fixed Property<CharSequence>.trimmed extension

--- properties/src/main/kotlin/net/aquadc/properties/charSequenceExtensions.kt
+++ properties/src/main/kotlin/net/aquadc/properties/charSequenceExtensions.kt
@@ -9,4 +9,4 @@ inline val Property<CharSequence>.isNotEmpty get() = map { it.isNotEmpty() }
 inline val Property<CharSequence>.isBlank get() = map { it.isBlank() }
 inline val Property<CharSequence>.isNotBlank get() = map { it.isNotBlank() }
 
-inline val Property<CharSequence>.trimmed get() = map { it.isNotBlank() }
+inline val Property<CharSequence>.trimmed get() = map { it.trim() }
