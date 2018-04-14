7b75e55c4bee4912fd6a56722a97d2f608799d4c
Miha_x64 <mihlo1996@rambler.ru>
2018-04-14 22:44:05 +0300

    refactored concPropUtils

--- properties/src/main/kotlin/net/aquadc/properties/internal/arrays.kt
+++ properties/src/main/kotlin/net/aquadc/properties/internal/arrays.kt
@@ -1,27 +1,5 @@
 package net.aquadc.properties.internal
 
-import java.util.concurrent.atomic.AtomicReferenceFieldUpdater
-
-
-// diff module contains the same thing, but it's internal, too
-internal inline fun <T, V> AtomicReferenceFieldUpdater<T, V>.update(zis: T, update: (V) -> V) {
-    var prev: V
-    var next: V
-    do {
-        prev = get(zis)
-        next = update(prev)
-    } while (!compareAndSet(zis, prev, next))
-}
-
-internal inline fun <T, V> AtomicReferenceFieldUpdater<T, V>.updateUndGet(zis: T, update: (V) -> V): V {
-    var prev: V
-    var next: V
-    do {
-        prev = get(zis)
-        next = update(prev)
-    } while (!compareAndSet(zis, prev, next))
-    return next
-}
 
 internal fun Array<*>.with(lastElement: Any?): Array<Any?> {
     val size = size
