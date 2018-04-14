6d29f8eaf8d0d36b9fece8c7edbf716aeff7b263
Miha_x64 <mihlo1996@rambler.ru>
2018-04-14 17:55:17 +0300

    added some implementation notes

--- properties/src/main/kotlin/net/aquadc/properties/Property.kt
+++ properties/src/main/kotlin/net/aquadc/properties/Property.kt
@@ -2,6 +2,8 @@ package net.aquadc.properties
 
 /**
  * Represents an observable value of type [T].
+ * If not concurrent, *every* method will throw [RuntimeException],
+ * including [isConcurrent] property getter.
  */
 interface Property<out T> {
 
@@ -12,23 +14,27 @@ interface Property<out T> {
 
     /**
      * When `true`, means that property value may change in future.
+     * This is `false` on immutable properties.
      */
     val mayChange: Boolean
 
     /**
-     * When `true`, means that property value can be
-     * mutated and observed from different threads.
+     * When `true`, this property can be touched from
+     * threads which are different from [Thread.currentThread].
+     * If `false`, this property is fully bound to current thread.
      */
     val isConcurrent: Boolean
 
     /**
      * Subscribe on value changes.
+     * Acts like [MutableList.add]: [onChange] will be added in any case.
      */
     fun addChangeListener(onChange: ChangeListener<T>)
 
     /**
      * Unsubscribe from value changes.
-     * If [onChange] is not registered as a listener, does nothing.
+     * Acts like [MutableList.remove]: removes only first occurrence of [onChange], if any.
+     * But checks equality by identity, not by [equals].
      */
     fun removeChangeListener(onChange: ChangeListener<T>)
 
