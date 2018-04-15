9c9bf6e6ede830ab44644c280092392fcfec4baa
Miha_x64 <mihlo1996@rambler.ru>
2018-04-15 20:25:03 +0300

    refactorings & renamings

--- properties/src/main/kotlin/net/aquadc/properties/executor/adapter.kt
+++ properties/src/main/kotlin/net/aquadc/properties/executor/adapter.kt
@@ -1,8 +1,9 @@
 package net.aquadc.properties.executor
 
 import net.aquadc.properties.ChangeListener
+import java.util.concurrent.Executor
 
-internal class MappedValueProxy<T, U>(
+internal class MapWhenChanged<in T, U>(
         private val mapOn: Worker,
         private val map: (T) -> U,
         private val consumer: (U) -> Unit
@@ -12,4 +13,15 @@ internal class MappedValueProxy<T, U>(
         mapOn.map(new, map, consumer)
     }
 
+}
+
+internal class ConsumeOn<in T>(
+        private val consumeOn: Executor,
+        private val consumer: (T) -> Unit
+) : (T) -> Unit {
+    override fun invoke(value: T) {
+        consumeOn.execute {
+            consumer(value)
+        }
+    }
 }
