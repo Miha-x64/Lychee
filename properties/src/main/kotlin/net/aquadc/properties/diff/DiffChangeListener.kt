74e39c69b131a8d57a925b9b4f32d25f4c7f78de
Miha_x64 <mihlo1996@rambler.ru>
2018-04-21 21:55:46 +0300

    merged DiffProperty utils into a single file/class

--- properties/src/main/kotlin/net/aquadc/properties/diff/DiffChangeListener.kt
+++
@@ -1,6 +0,0 @@
-package net.aquadc.properties.diff
-
-/**
- * Observer for [DiffProperty]<T, D>.
- */
-typealias DiffChangeListener<T, D> = (old: T, new: T, diff:D) -> Unit
--- properties/src/main/kotlin/net/aquadc/properties/diff/factory.kt
+++
@@ -1,9 +0,0 @@
-@file:Suppress("NOTHING_TO_INLINE")
-
-package net.aquadc.properties.diff
-
-import net.aquadc.properties.diff.internal.ConcMutableDiffProperty
-
-
-inline fun <T, D> concurrentMutableDiffPropertyOf(value: T): MutableDiffProperty<T, D> =
-        ConcMutableDiffProperty(value)
