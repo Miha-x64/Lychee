588934c0714aea0810107e957d672fbb4a052d74
Miha_x64 <mihlo1996@rambler.ru>
2018-05-02 12:09:15 +0300

    KDoc on FX bindTo

--- fx-bindings/src/main/kotlin/net/aquadc/properties/fx/bindTo.kt
+++ fx-bindings/src/main/kotlin/net/aquadc/properties/fx/bindTo.kt
@@ -3,6 +3,9 @@ package net.aquadc.properties.fx
 import net.aquadc.properties.Property
 import javafx.beans.property.Property as FxProperty
 
+/**
+ * Binds [this] property value to [that].
+ */
 fun <T> FxProperty<in T>.bindTo(that: Property<T>) {
     this.value = that.value
     that.addChangeListener { _, new ->
