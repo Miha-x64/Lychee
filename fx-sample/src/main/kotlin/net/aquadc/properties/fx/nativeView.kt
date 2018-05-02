bb62e097c45f11898d94d744c90f932225422eed
Miha_x64 <mihlo1996@rambler.ru>
2018-05-02 13:50:05 +0300

    fx: Presenter -> FxViewModel

--- fx-sample/src/main/kotlin/net/aquadc/properties/fx/nativeView.kt
+++ fx-sample/src/main/kotlin/net/aquadc/properties/fx/nativeView.kt
@@ -5,29 +5,29 @@ import com.jfoenix.controls.JFXTextField
 import javafx.geometry.Insets
 import javafx.scene.layout.VBox
 
-fun nativeView(presenter: Presenter) = VBox(10.0).apply {
+fun nativeView(viewModel: FxViewModel) = VBox(10.0).apply {
 
     padding = Insets(10.0, 10.0, 10.0, 10.0)
 
     children.add(JFXTextField().apply {
         promptText = "Email"
-        textProperty().bindBidirectional(presenter.emailProp)
+        textProperty().bindBidirectional(viewModel.emailProp)
     })
 
     children.add(JFXTextField().apply {
         promptText = "Name"
-        textProperty().bindBidirectional(presenter.nameProp)
+        textProperty().bindBidirectional(viewModel.nameProp)
     })
 
     children.add(JFXTextField().apply {
         promptText = "Surname"
-        textProperty().bindBidirectional(presenter.surnameProp)
+        textProperty().bindBidirectional(viewModel.surnameProp)
     })
 
     children.add(JFXButton("Press me, hey, you!").apply {
-        disableProperty().bind(!presenter.buttonEnabledProp)
-        textProperty().bind(presenter.buttonTextProp)
-        setOnAction { presenter.saveButtonClicked() }
+        disableProperty().bind(!viewModel.buttonEnabledProp)
+        textProperty().bind(viewModel.buttonTextProp)
+        setOnAction { viewModel.saveButtonClicked() }
     })
 
 }
