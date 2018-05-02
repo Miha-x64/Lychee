9611bcdf4158582154ca4896ffea6dc492da5fed
Miha_x64 <mihlo1996@rambler.ru>
2018-05-02 12:13:18 +0300

    KDocs for FX bindings

--- fx-bindings/src/main/kotlin/net/aquadc/properties/fx/Node.kt
+++ fx-bindings/src/main/kotlin/net/aquadc/properties/fx/Node.kt
@@ -3,10 +3,16 @@ package net.aquadc.properties.fx
 import javafx.scene.Node
 import net.aquadc.properties.Property
 
+/**
+ * Binds [Node.visibleProperty] to [visibleProperty].
+ */
 fun Node.bindVisibilitySoftlyTo(visibleProperty: Property<Boolean>) {
     visibleProperty().bindTo(visibleProperty)
 }
 
+/**
+ * Binds [Node.visibleProperty] and [Node.managedProperty] to [visibleProperty].
+ */
 fun Node.bindVisibilityHardlyTo(visibleProperty: Property<Boolean>) {
     // As suggested in https://stackoverflow.com/a/28559958/3050249
     managedProperty().bindTo(visibleProperty)
--- fx-bindings/src/main/kotlin/net/aquadc/properties/fx/bindBidirectionally.kt
+++ fx-bindings/src/main/kotlin/net/aquadc/properties/fx/bindBidirectionally.kt
@@ -3,6 +3,10 @@ package net.aquadc.properties.fx
 import net.aquadc.properties.MutableProperty
 import javafx.beans.property.Property as FxProperty
 
+/**
+ * Binds [this] and [that] property values.
+ * [this] takes [that] value first.
+ */
 fun <T> FxProperty<T>.bindBidirectionally(that: MutableProperty<T>) {
     this.value = that.value
     var mutatingThis = false
