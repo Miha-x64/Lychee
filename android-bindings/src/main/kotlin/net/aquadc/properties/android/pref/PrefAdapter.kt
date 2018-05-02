110b51edd7e8bafcf2a5c357a5fa4f81a1d37717
Miha_x64 <mihlo1996@rambler.ru>
2018-05-02 12:28:21 +0300

    KDocs for PrefAdapter

--- android-bindings/src/main/kotlin/net/aquadc/properties/android/pref/PrefAdapter.kt
+++ android-bindings/src/main/kotlin/net/aquadc/properties/android/pref/PrefAdapter.kt
@@ -6,12 +6,30 @@ import android.content.SharedPreferences
  * Generic adapter to SharedPreferences#get* and SharedPreferences.Editor#set*.
  */
 interface PrefAdapter<T> {
+
+    /**
+     * Reads and returns a value from [prefs] by [key].
+     * Returns [default] if there's no such mapping.
+     */
     fun read(prefs: SharedPreferences, key: String, default: T): T
+
+    /**
+     * Maps [key] to [value] in [editor].
+     * Must not call [SharedPreferences.Editor.apply] or [SharedPreferences.Editor.commit].
+     */
     fun save(editor: SharedPreferences.Editor, key: String, value: T)
-    // Interesting one.
-    // When you're holding several values (e. g. username and email), say, in ${propKey}_name and ${propKey}_email props
-    // and writing them in a single transaction, you should return true for each property,
-    // e. g. for `isKeyFor("someKey", "someKey_name" or "someKey_email")` to help property keeping data up to date.
+
+    /**
+     * Checks whether [propKey] is for [prefKey].
+     * If [read] and [save] functions transform passed keys somehow,
+     * should implement some checking.
+     *
+     * For example, when a single [PrefAdapter] stands for several [SharedPreferences] values,
+     * e. g. username and email, say, as `${propKey}_name` and `${propKey}_email`,
+     * then it should return `true` for each property,
+     * i. e. both `isKeyFor("someKey", "someKey_name" and "someKey_email")`,
+     * to help properties keeping data up to date.
+     */
     fun isKeyFor(propKey: String, prefKey: String): Boolean
 }
 
