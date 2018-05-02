ad807581c2482c79567c1d7b134db3b4bbe30f62
Miha_x64 <mihlo1996@rambler.ru>
2018-05-02 10:27:14 +0300

    moved MessageAndIconRes into TextView.kt

--- android-bindings/src/main/kotlin/net/aquadc/properties/android/container/MessageAndIconRes.kt
+++
@@ -1,9 +0,0 @@
-package net.aquadc.properties.android.container
-
-import android.support.annotation.DrawableRes
-import android.support.annotation.StringRes
-
-class MessageAndIconRes(
-        @JvmField @StringRes val messageRes: Int,
-        @JvmField @DrawableRes val iconRes: Int
-)
