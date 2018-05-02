911dacfe0454b71d06937c4765fefaa0e320913a
Miha_x64 <mihlo1996@rambler.ru>
2018-05-02 12:09:31 +0300

    KDocs on Simple*/adapters

--- android-bindings/src/main/kotlin/net/aquadc/properties/android/simple/design/SimpleOnTabSelectedListener.kt
+++ android-bindings/src/main/kotlin/net/aquadc/properties/android/simple/design/SimpleOnTabSelectedListener.kt
@@ -2,7 +2,9 @@ package net.aquadc.properties.android.simple.design
 
 import android.support.design.widget.TabLayout
 
-
+/**
+ * Simple adapter class for [TabLayout.OnTabSelectedListener].
+ */
 open class SimpleOnTabSelectedListener : TabLayout.OnTabSelectedListener {
     override fun onTabReselected(tab: TabLayout.Tab) = Unit
     override fun onTabUnselected(tab: TabLayout.Tab) = Unit
