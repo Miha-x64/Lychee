3681152c59dc070cd40a192ef4fbe2a3358afda8
Miha_x64 <mihlo1996@rambler.ru>
2018-06-13 23:20:33 +0300

    added Handlecutor — Handler & Executor in one bottle

--- properties/src/main/kotlin/net/aquadc/properties/executor/platform.kt
+++ properties/src/main/kotlin/net/aquadc/properties/executor/platform.kt
@@ -1,6 +1,7 @@
 package net.aquadc.properties.executor
 
 import android.os.Handler
+import android.os.Looper
 import javafx.application.Platform
 import java.util.concurrent.Executor
 
@@ -18,7 +19,19 @@ class HandlerAsExecutor(
 }
 
 /**
+ * [Executor] implementation merged with Android's [Handler].
+ * Will cause [NoClassDefFoundError] if called out of Android.
+ */
+class Handlecutor(looper: Looper) : Handler(looper), Executor {
+
+    override fun execute(command: Runnable) =
+            check(post(command))
+
+}
+
+/**
  * Wraps [Platform] to run JavaFX Application Thread.
+ * Will cause [NoClassDefFoundError] if called on platform without JavaFX.
  */
 object FxApplicationThreadExecutor : Executor {
 
