27ffa9898ac1d5ac2e3b651da82a162a4baf37dd
Miha_x64 <mihlo1996@rambler.ru>
2018-08-12 01:00:11 +0300

    re-binding sequentially, potentially fixes #40

--- properties/src/main/kotlin/net/aquadc/properties/internal/ConcListeners.kt
+++ properties/src/main/kotlin/net/aquadc/properties/internal/ConcListeners.kt
@@ -12,11 +12,12 @@ internal class ConcListeners<out L : Any, out T>(
         @JvmField val listeners: Array<out L?>,
         @JvmField val pendingValues: Array<out T>,
         @JvmField val transitioningObservedState: Boolean,
-        @JvmField val nextObservedState: Boolean
+        @JvmField val nextObservedState: Boolean,
+        @JvmField val transitionLocked: Boolean
 ) {
 
     fun withListener(newListener: @UnsafeVariance L): ConcListeners<L, T> =
-            ConcListeners(listeners.with(newListener) as Array<L?>, pendingValues, transitioningObservedState, nextObservedState)
+            ConcListeners(listeners.with(newListener) as Array<L?>, pendingValues, transitioningObservedState, nextObservedState, transitionLocked)
 
     fun withoutListenerAt(idx: Int): ConcListeners<L, T> {
         val newListeners = when {
@@ -36,11 +37,11 @@ internal class ConcListeners<out L : Any, out T>(
         return if (pendingValues.isEmpty() && newListeners.isEmpty() && !transitioningObservedState && !nextObservedState)
             NoListeners
         else
-            ConcListeners(newListeners, pendingValues, transitioningObservedState, nextObservedState)
+            ConcListeners(newListeners, pendingValues, transitioningObservedState, nextObservedState, transitionLocked)
     }
 
     fun withNextValue(newValue: @UnsafeVariance T): ConcListeners<L, T> =
-            ConcListeners(listeners, pendingValues.with(newValue) as Array<out T>, transitioningObservedState, nextObservedState)
+            ConcListeners(listeners, pendingValues.with(newValue) as Array<out T>, transitioningObservedState, nextObservedState, transitionLocked)
 
     fun next(): ConcListeners<L, T> {
         val listeners = if (this.pendingValues.size == 1) {
@@ -51,17 +52,20 @@ internal class ConcListeners<out L : Any, out T>(
         }
 
         // remove value at 0, that listeners were just notified about
-        return ConcListeners(listeners, pendingValues.copyOfWithout(0, EmptyArray) as Array<out T>, transitioningObservedState, nextObservedState)
+        return ConcListeners(listeners, pendingValues.copyOfWithout(0, EmptyArray) as Array<out T>,
+                transitioningObservedState, nextObservedState, transitionLocked)
     }
 
-    fun startTransition(): ConcListeners<L, T> {
-        return ConcListeners(listeners, pendingValues, true, !nextObservedState)
-    }
+    fun startTransition(): ConcListeners<L, T> =
+            ConcListeners(listeners, pendingValues, true, !nextObservedState, transitionLocked)
 
     fun continueTransition(appliedState: Boolean): ConcListeners<L, T> {
         check(transitioningObservedState)
         val done = appliedState == nextObservedState
-        return ConcListeners(listeners, pendingValues, !done, nextObservedState)
+        return ConcListeners(listeners, pendingValues, !done, nextObservedState, transitionLocked)
     }
+
+    fun flippedTransitionLock(): ConcListeners<L, T> =
+            ConcListeners(listeners, pendingValues, transitioningObservedState, nextObservedState, !transitionLocked)
 
 }
