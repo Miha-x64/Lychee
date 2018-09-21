4e4296b0c20bb1c603c0276755ce95d93196b8b2
Miha_x64 <mihlo1996@rambler.ru>
2018-09-21 22:25:28 +0300

    added Property<List>.+=, renamed MapExtensions class to CollectionExtensions

--- properties/src/main/kotlin/net/aquadc/properties/collectionExtensions.kt
+++ properties/src/main/kotlin/net/aquadc/properties/collectionExtensions.kt
@@ -1,4 +1,4 @@
-@file:JvmName("MapProperties")
+@file:JvmName("CollectionProperties")
 package net.aquadc.properties
 
 
@@ -12,6 +12,16 @@ operator fun <K, V> MutableProperty<Map<K, V>>.plusAssign(newMapping: Pair<K, V>
         if (newMap === map) return // already contains this mapping
     } while (!casValue(map, newMap))
 }
+
+/**
+ * Assigns `this.value + newElement` to current property's value.
+ */
+operator fun <T> MutableProperty<List<T>>.plusAssign(newElement: T) {
+    do {
+        val list = value
+    } while (!casValue(list, list + newElement))
+}
+
 
 // TODO: minusAssign, etc
 // TODO: move collection extensions to separate module, maybe along with `listOf(...).map { ... }`
