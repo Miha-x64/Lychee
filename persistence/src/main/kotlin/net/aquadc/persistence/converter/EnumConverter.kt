8c34d3cb3e4d373ae2f5c6f1d6e215bda2ba8e1c
atomic-mike <atomic-mike@yandex-team.ru>
2018-09-23 09:46:18 +0300

    persistence: added DelegatingConverter, re-implemented EnumConverter on top of it

--- persistence/src/main/kotlin/net/aquadc/persistence/converter/EnumConverter.kt
+++ persistence/src/main/kotlin/net/aquadc/persistence/converter/EnumConverter.kt
@@ -1,58 +1,14 @@
 package net.aquadc.persistence.converter
 
-import android.database.Cursor
-import android.database.sqlite.SQLiteStatement
-import android.os.Parcel
-import java.io.DataInput
-import java.io.DataOutput
-import java.sql.PreparedStatement
-import java.sql.ResultSet
-
+@Suppress("RedundantModalityModifier")
 @PublishedApi internal open class EnumConverter<E : Enum<E>>(
-        private val enumType: Class<E>,
-        dataType: DataType
-) : SimpleConverter<E>(dataType, false) {
+        private val enumType: Class<E>
+) : DelegatingConverter<E, String>(string) {
 
-    // JDBC
-
-    final override fun bind(statement: PreparedStatement, index: Int, value: E) {
-        string.bind(statement, index, asString(value))
-    }
-
-    final override fun get(resultSet: ResultSet, index: Int): E =
-            lookup(string.get(resultSet, index))
-
-    // Android SQLite
-
-    final override fun bind(statement: SQLiteStatement, index: Int, value: E) {
-        string.bind(statement, index, asString(value))
-    }
-
-    @Suppress("RedundantModalityModifier")
     open override fun asString(value: E): String = value.name
 
-    final override fun get(cursor: Cursor, index: Int): E = lookup(cursor.getString(index))
+    open override fun from(value: String): E = java.lang.Enum.valueOf<E>(enumType, value)
 
-    // IO streams
-
-    final override fun write(output: DataOutput, value: E) {
-        string.write(output, asString(value))
-    }
-
-    final override fun read(input: DataInput): E =
-            lookup(string.read(input))
-
-    // Parcel
-
-    final override fun write(destination: Parcel, value: E) {
-        string.write(destination, asString(value))
-    }
-
-    final override fun read(source: Parcel): E =
-            lookup(string.read(source))
-
-    // own
-
-    protected open fun lookup(name: String): E = java.lang.Enum.valueOf<E>(enumType, name)
+    final override fun E.to(): String = asString(this)
 
 }
