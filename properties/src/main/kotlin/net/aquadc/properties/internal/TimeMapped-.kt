891a0805e5f73d7e551ebe858205702532629af1
Miha_x64 <mihlo1996@rambler.ru>
2018-10-02 23:15:19 +0300

    more accuracy in updatedEvery() thanks to scheduleAtFixedRate

--- properties/src/main/kotlin/net/aquadc/properties/internal/TimeMapped-.kt
+++ properties/src/main/kotlin/net/aquadc/properties/internal/TimeMapped-.kt
@@ -4,33 +4,32 @@ import net.aquadc.properties.Property
 import net.aquadc.properties.executor.InPlaceWorker
 import net.aquadc.properties.executor.ScheduledDaemonHolder
 import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.ScheduledFuture
 
 @PublishedApi
 internal class `TimeMapped-`<O, T>(
         original: Property<O>,
         map: (O) -> T,
-        private val scheduleNext: (ScheduledExecutorService, T, Runnable) -> Unit
-) : `Mapped-`<O, T>(original, map, InPlaceWorker) {
+        private val schedulePeriodically: (ScheduledExecutorService, T, Runnable) -> ScheduledFuture<*>
+) : `Mapped-`<O, T>(original, map, InPlaceWorker), Runnable {
 
-    private val periodical = object : Runnable {
-        override fun run() {
-            if (isBeingObserved()) {
-                val value = original.value
-                originalChanged(value, value) // impl detail: 'old' will be ignored
-                val ref = ref
-                if (ref !== unset<T>()) {
-                    scheduleNext(ScheduledDaemonHolder.scheduledDaemon, ref, this)
-                }
-            }
+    override fun run() {
+        if (isBeingObserved()) {
+            val value = original.value
+            originalChanged(unset(), value) // impl detail: 'old' will be ignored
         }
     }
+
+    @Volatile
+    private var future: ScheduledFuture<*>? = null
 
     override fun observedStateChanged(observed: Boolean) {
         super.observedStateChanged(observed)
         val ref = ref
-        if (observed && ref !== unset<T>()) {
-            scheduleNext(ScheduledDaemonHolder.scheduledDaemon, ref, periodical)
-        }
+        future = if (observed && ref !== unset<T>())
+            schedulePeriodically(ScheduledDaemonHolder.scheduledDaemon, ref, this)
+        else
+            future!!.cancel(false).let { null }
     }
 
 }
