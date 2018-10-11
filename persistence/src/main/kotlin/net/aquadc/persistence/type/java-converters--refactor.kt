a651972cf8fe501655ae1885ed98d6151221661c
Miha-x64 <mihlo1996@rambler.ru>
2018-10-11 18:19:16 +0300

    moved PropertyRW/IO from Converters to Types

--- persistence/src/main/kotlin/net/aquadc/persistence/type/java-converters--refactor.kt
+++ persistence/src/main/kotlin/net/aquadc/persistence/type/java-converters--refactor.kt
@@ -1,19 +1,12 @@
 package net.aquadc.persistence.type
 
 import android.content.SharedPreferences
-import android.database.Cursor
-import android.database.sqlite.SQLiteStatement
 import android.util.Base64
-import net.aquadc.persistence.stream.CleverDataInput
-import net.aquadc.persistence.stream.CleverDataOutput
 import net.aquadc.persistence.struct.t
 import java.lang.Double.doubleToLongBits
 import java.lang.Double.longBitsToDouble
 import java.lang.Float.floatToIntBits
 import java.lang.Float.intBitsToFloat
-import java.sql.PreparedStatement
-import java.sql.ResultSet
-import java.sql.Types
 
 
 internal abstract class SimpleConverter<T>(
@@ -26,118 +19,6 @@ private class BasicConverter<T>(
         dataType: DataType,
         isNullable: Boolean
 ) : SimpleConverter<T>(dataType, isNullable) {
-
-    // JDBC
-
-    override fun bind(statement: PreparedStatement, index: Int, value: T) {
-        val i = 1 + index
-        when (value) {
-            is Boolean, is Short, is Int, is Long, is Float, is Double,
-            is String,
-            is ByteArray -> statement.setObject(i, value)
-            is Byte -> statement.setInt(i, value.toInt())
-            null -> statement.setNull(i, Types.NULL)
-            else -> throw AssertionError()
-        }
-    }
-
-    override fun get(resultSet: ResultSet, index: Int): T {
-        val i = 1 + index
-        @Suppress("IMPLICIT_CAST_TO_ANY", "UNCHECKED_CAST")
-        return when (javaType) {
-            t<Boolean>() -> resultSet.getBoolean(i)
-            t<Byte>() -> resultSet.getByte(i)
-            t<Short>() -> resultSet.getShort(i)
-            t<Int>() -> resultSet.getInt(i)
-            t<Long>() -> resultSet.getLong(i)
-            t<Float>() -> resultSet.getFloat(i)
-            t<Double>() -> resultSet.getDouble(i)
-            t<String>() -> resultSet.getString(i)
-            t<ByteArray>() -> resultSet.getBytes(i)
-            else -> throw AssertionError()
-        } as T
-    }
-
-    // Android SQLite
-
-    override fun bind(statement: SQLiteStatement, index: Int, value: T) {
-        val i = 1 + index
-        when (value) {
-            is Boolean -> statement.bindLong(i, if (value) 1 else 0)
-            is Byte, is Short, is Int, is Long -> statement.bindLong(i, (value as Number).toLong())
-            is Float, is Double -> statement.bindDouble(i, (value as Number).toDouble())
-            is String -> statement.bindString(i, value)
-            is ByteArray -> statement.bindBlob(i, value)
-            null -> statement.bindNull(i)
-            else -> throw AssertionError()
-        }
-    }
-
-    override fun asString(value: T): String =
-            value.toString()
-
-    @Suppress("IMPLICIT_CAST_TO_ANY", "UNCHECKED_CAST")
-    override fun get(cursor: Cursor, index: Int): T = when (javaType) {
-        t<Boolean>() -> cursor.getInt(index) != 0
-        t<Byte>() -> cursor.getShort(index).assertFitsByte()
-        t<Short>() -> cursor.getShort(index)
-        t<Int>() -> cursor.getInt(index)
-        t<Long>() -> cursor.getLong(index)
-        t<Float>() -> cursor.getFloat(index)
-        t<Double>() -> cursor.getDouble(index)
-        t<String>() -> cursor.getString(index)
-        t<ByteArray>() -> cursor.getBlob(index)
-        else -> throw AssertionError()
-    } as T
-
-    // IO streams
-
-    override fun write(output: CleverDataOutput, value: T) {
-        when (javaType) {
-            t<String>() -> return output.writeString(value as String?)
-            t<ByteArray>() -> return output.writeBytes(value as ByteArray?)
-            t<Boolean>() -> return output.writeByte((value as Boolean?).asByte().toInt())
-        }
-
-        if (isNullable) {
-            val isNull = value == null
-            output.writeByte(if (isNull) -1 else 0)
-            if (isNull) return
-        }
-
-        when (value) {
-            is Byte -> output.writeByte(value.toInt())
-            is Short -> output.writeShort(value.toInt())
-            is Int -> output.writeInt(value)
-            is Long -> output.writeLong(value)
-            is Float -> output.writeInt(floatToIntBits(value))
-            is Double -> output.writeLong(doubleToLongBits(value))
-            else -> throw AssertionError()
-        }
-    }
-
-    @Suppress("IMPLICIT_CAST_TO_ANY", "UNCHECKED_CAST")
-    override fun read(input: CleverDataInput): T {
-        when (javaType) {
-            t<String>() -> return input.readString() as T
-            t<ByteArray>() -> return input.readBytes() as T
-            t<Boolean>() -> return input.readByte().asBoolean() as T
-        }
-
-        if (isNullable && input.readByte() == (-1).toByte()) {
-            return null as T
-        }
-
-        return when (javaType) {
-            t<Byte>() -> input.readByte()
-            t<Short>() -> input.readShort()
-            t<Int>() -> input.readInt()
-            t<Long>() -> input.readLong()
-            t<Float>() -> intBitsToFloat(input.readInt())
-            t<Double>() -> longBitsToDouble(input.readLong())
-            else -> throw AssertionError()
-        } as T
-    }
 
     // Prefs
 
@@ -211,11 +92,7 @@ private class BasicConverter<T>(
 
     // util
 
-    private fun Short.assertFitsByte(): Byte {
-        if (this !in Byte.MIN_VALUE..Byte.MAX_VALUE)
-            throw IllegalStateException("value ${this} cannot be fit into a byte")
-        return toByte()
-    }
+
 
     private fun Int.assertFitsByte(): Byte {
         if (this !in Byte.MIN_VALUE..Byte.MAX_VALUE)
