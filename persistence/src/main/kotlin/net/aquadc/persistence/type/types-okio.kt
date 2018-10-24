6057313ea762269c150ca2d0cf1bf8666112e194
Miha-x64 <mihlo1996@rambler.ru>
2018-10-24 19:51:36 +0300

    fixed nullability flags in persistence/types

--- persistence/src/main/kotlin/net/aquadc/persistence/type/types-okio.kt
+++ persistence/src/main/kotlin/net/aquadc/persistence/type/types-okio.kt
@@ -19,13 +19,13 @@ private class ByteStr<T : ByteString?>(
 
 /*
 @JvmField val smallByteString: DataType<ByteString> = ByteStr(false, Byte.MAX_VALUE.toInt())
-@JvmField val smallNullableByteString: DataType<ByteString?> = ByteStr(false, Byte.MAX_VALUE.toInt())
+@JvmField val smallNullableByteString: DataType<ByteString?> = ByteStr(true, Byte.MAX_VALUE.toInt())
 
 @JvmField val mediumByteString: DataType<ByteString> = ByteStr(false, Short.MAX_VALUE.toInt())
-@JvmField val mediumNullableByteString: DataType<ByteString?> = ByteStr(false, Short.MAX_VALUE.toInt())
+@JvmField val mediumNullableByteString: DataType<ByteString?> = ByteStr(true, Short.MAX_VALUE.toInt())
 
 @JvmField val largeByteString: DataType<ByteString> = ByteStr(false, Int.MAX_VALUE)
-@JvmField val largeNullableByteString: DataType<ByteString?> = ByteStr(false, Int.MAX_VALUE)
+@JvmField val largeNullableByteString: DataType<ByteString?> = ByteStr(true, Int.MAX_VALUE)
 */
 @JvmField val byteString: DataType<ByteString> = ByteStr(false, Int.MAX_VALUE)
 @JvmField val nullableByteString: DataType<ByteString?> = ByteStr(true, Int.MAX_VALUE)
