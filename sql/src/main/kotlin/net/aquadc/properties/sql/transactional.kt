a685528359333aa1a501d1e9a7191da8853c2043
Miha-x64 <mihlo1996@rambler.ru>
2018-10-29 20:18:57 +0300

    added Transactional adapter for Records (SQL #32)

---
+++ sql/src/main/kotlin/net/aquadc/properties/sql/transactional.kt
@@ -0,0 +1,69 @@
+package net.aquadc.properties.sql
+
+import net.aquadc.persistence.struct.FieldDef
+import net.aquadc.persistence.struct.StructTransaction
+import net.aquadc.properties.TransactionalProperty
+import net.aquadc.properties.internal.ManagedProperty
+import net.aquadc.properties.internal.Manager
+import net.aquadc.properties.internal.Unset
+import net.aquadc.properties.persistence.TransactionalPropertyStruct
+
+
+@PublishedApi internal class RecordTransactionalAdapter<TBL : Table<TBL, ID, REC>, ID : IdBound, REC : Record<TBL, ID>>(
+        private val record: REC
+) : TransactionalPropertyStruct<TBL> {
+
+    override val type: TBL
+        get() = record.type
+
+    override fun <T> get(field: FieldDef<TBL, T>): T =
+            record[field]
+
+    // places for immutable fields remain nulls,
+    // places for mutable are occupied by our wrappers
+    // TODO: smaller array size :)
+    private val props = arrayOfNulls<TransactionalProperty<StructTransaction<TBL>, *>>(record.type.fields.size)
+
+    override fun <T> prop(field: FieldDef.Mutable<TBL, T>): TransactionalProperty<StructTransaction<TBL>, T> {
+        val index = field.ordinal.toInt()
+        return (props[index] as? ManagedProperty<TBL, StructTransaction<TBL>, T>)
+                ?: ManagedProperty(manager, field, -1, Unset as T).also { props[index] = it }
+    }
+
+    private val manager = object : Manager<TBL, StructTransaction<TBL>> {
+
+        override fun <T> getDirty(field: FieldDef.Mutable<TBL, T>, id: Long): T =
+                Unset as T
+
+        override fun <T> getClean(field: FieldDef.Mutable<TBL, T>, id: Long): T =
+                record[field]
+
+        override fun <T> set(transaction: StructTransaction<TBL>, field: FieldDef.Mutable<TBL, T>, id: Long, update: T) {
+            transaction.set(field, update)
+        }
+
+    }
+
+    override fun beginTransaction(): StructTransaction<TBL> = object : StructTransaction<TBL> {
+
+        private val transaction = record.session.beginTransaction()
+
+        override fun <T> set(field: FieldDef.Mutable<TBL, T>, update: T) =
+                record.prop(field).setValue(transaction, update)
+
+        override fun setSuccessful() {
+            transaction.setSuccessful()
+        }
+
+        override fun close() {
+            transaction.close()
+        }
+
+    }
+
+}
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun <TBL : Table<TBL, ID, REC>, ID : IdBound, REC : Record<TBL, ID>>
+        REC.transactional(): TransactionalPropertyStruct<TBL> =
+        RecordTransactionalAdapter(this)
