6556c3fb068aa5dffb1143f69cfdf36b17d70bee
Miha-x64 <mihlo1996@rambler.ru>
2018-11-23 20:23:08 +0300

    [FX sample] fixed imports after moving .transactional()

--- fx-sample/src/main/kotlin/net/aquadc/properties/fx/FxWithOurPropsSample.kt
+++ fx-sample/src/main/kotlin/net/aquadc/properties/fx/FxWithOurPropsSample.kt
@@ -4,7 +4,6 @@ import javafx.application.Application
 import javafx.scene.Scene
 import javafx.stage.Stage
 import net.aquadc.properties.persistence.ObservableStruct
-import net.aquadc.properties.persistence.transactional
 import net.aquadc.propertiesSampleLogic.MainVm
 import net.aquadc.propertiesSampleLogic.User
 
