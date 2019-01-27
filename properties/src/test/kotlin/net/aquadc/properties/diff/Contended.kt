cc91ea50cb4b44508941e0d6ec2b4d311f03b6af
Miha_x64 <mihlo1996@rambler.ru>
2019-01-27 16:33:23 +0300

    added a test on CASes during notification

--- properties/src/test/kotlin/net/aquadc/properties/diff/Contended.kt
+++ properties/src/test/kotlin/net/aquadc/properties/diff/Contended.kt
@@ -1,11 +1,14 @@
 package net.aquadc.properties.diff
 
+import net.aquadc.properties.addUnconfinedChangeListener
+import net.aquadc.properties.concurrentPropertyOf
 import org.junit.Assert.assertEquals
 import org.junit.AssumptionViolatedException
 import org.junit.Test
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.concurrent.thread
 
 
 class Contended {
@@ -39,7 +42,7 @@ class Contended {
             sum.addAndGet(diff!!)
         }
 
-        repeat(100_000) {
+        repeat(10_000) {
             pool.execute {
                 try {
                     do {
@@ -57,5 +60,28 @@ class Contended {
         check(pool.awaitTermination(60, TimeUnit.SECONDS))
         assertEquals(100_000, sum.get())
     }
+
+    @Test fun casDuringNotification() {
+        val prop = concurrentPropertyOf(0)
+        prop.addUnconfinedChangeListener { _, _ -> Thread.sleep(100) }
+        val v0s = concurrentPropertyOf(false)
+        thread {
+            val v0 = prop.value
+            v0s.value = prop.casValue(v0, v0 + 10)
+        }
+        val v1 = prop.value
+        val v1s = prop.casValue(v1, v1 + 10)
+
+        val v2 = prop.value
+        val v2s = prop.casValue(v2, v2 + 10)
+
+        val v3 = prop.value
+        val v3s = prop.casValue(v3, v3 + 10)
+
+        assertEquals(10 * (v0s.value.i + v1s.i + v2s.i + v3s.i), prop.value)
+    }
+
+    private val Boolean.i
+        get() = if (this) 1 else 0
 
 }
