26ac5783803cf6e9ee4b7496c8ac9f9451dc0cd6
Miha_x64 <mihlo1996@rambler.ru>
2020-03-14 13:24:23 +0300

    TemplatesTest checks for hasNext correctness

--- sql/src/test/kotlin/net/aquadc/persistence/sql/TemplatesTest.kt
+++ sql/src/test/kotlin/net/aquadc/persistence/sql/TemplatesTest.kt
@@ -119,10 +119,19 @@ open class TemplatesTest {
                     USERS_BY_NAME_AND_EMAIL_START, string, string,
                     structs<CUR, Tuple<Struct<Tuple<String, DataType.Simple<String>, String, DataType.Simple<String>>>, Tuple<String, DataType.Simple<String>, String, DataType.Simple<String>>, Struct<Tuple<String, DataType.Simple<String>, Long, DataType.Simple<Long>>>, Tuple<String, DataType.Simple<String>, Long, DataType.Simple<Long>>>>(joined, BindBy.Name)
             )
-            val iter = userContacts("John", "john") // don't collect TemporaryStructs!
-            assertTrue(iter.hasNext())
-            assertEquals(expectedJohn, iter.next())
-            assertFalse(iter.hasNext())
+            userContacts("John", "john").use { iter -> // don't collect TemporaryStructs!
+                assertEquals(expectedJohn, iter.next())
+                assertFalse(iter.hasNext())
+            }
+
+            userContacts("John", "john").use { iter ->
+                assertTrue(iter.hasNext()) // any number of `hasNext`s must be OK
+                assertTrue(iter.hasNext())
+                assertTrue(iter.hasNext())
+                assertTrue(iter.hasNext())
+                assertEquals(expectedJohn, iter.next())
+                assertFalse(iter.hasNext())
+            }
         }
     }
 
