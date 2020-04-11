1b3a456037e7444467fca79774a4b0e0e3d0fa82
Miha_x64 <mihlo1996@rambler.ru>
2020-04-11 19:44:35 +0300

    EitherTypes: hidden + operator overload for Java code

--- extended-persistence/src/main/kotlin/net/aquadc/persistence/extended/either/eitherTypes.kt
+++ extended-persistence/src/main/kotlin/net/aquadc/persistence/extended/either/eitherTypes.kt
@@ -49,6 +49,7 @@ fun <A, DA : DataType<A>, B, DB : DataType<B>> either(
 ): DataType.Partial<Either<A, B>, Tuple<A, DA, B, DB>> =
         EitherType(Tuple(firstName, firstType, secondName, secondType))
 
+@JvmSynthetic // useless for Java
 operator fun <A, DA : DataType<A>, B, DB : DataType<B>>
         DA.plus(second: DB): DataType.Partial<Either<A, B>, Tuple<A, DA, B, DB>> =
         either("first", this, "second", second)
@@ -63,6 +64,7 @@ fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>> either3(
                 firstName, firstType, secondName, secondType, thirdName, thirdType
         ))
 
+@JvmSynthetic // useless for Java
 operator fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>>
         DataType.Partial<Either<A, B>, Tuple<A, DA, B, DB>>.plus(third: DC): DataType.Partial<Either3<A, B, C>, Tuple3<A, DA, B, DB, C, DC>> =
         either3("first", schema.First.type, "second", schema.Second.type, "third", third)
@@ -78,6 +80,7 @@ fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : Data
                 firstName, firstType, secondName, secondType, thirdName, thirdType, fourthName, fourthType
         ))
 
+@JvmSynthetic // useless for Java
 @JvmName("e3plus") operator fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : DataType<D>>
         DataType.Partial<Either3<A, B, C>, Tuple3<A, DA, B, DB, C, DC>>.plus(fourth: DD): DataType.Partial<Either4<A, B, C, D>, Tuple4<A, DA, B, DB, C, DC, D, DD>> =
         either4("first", schema.First.type, "second", schema.Second.type, "third", schema.Third.type, "fourth", fourth)
@@ -95,6 +98,7 @@ fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : Data
                 fifthName, fifthType
         ))
 
+@JvmSynthetic // useless for Java
 @JvmName("e4plus") operator fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : DataType<D>, E, DE : DataType<E>>
         DataType.Partial<Either4<A, B, C, D>, Tuple4<A, DA, B, DB, C, DC, D, DD>>.plus(fifth: DE): DataType.Partial<Either5<A, B, C, D, E>, Tuple5<A, DA, B, DB, C, DC, D, DD, E, DE>> =
         either5("first", schema.First.type, "second", schema.Second.type, "third", schema.Third.type, "fourth", schema.Fourth.type, "fifth", fifth)
@@ -113,6 +117,7 @@ fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : Data
                 fifthName, fifthType, sixthName, sixthType
         ))
 
+@JvmSynthetic // useless for Java
 @JvmName("e5plus") operator fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : DataType<D>, E, DE : DataType<E>, F, DF : DataType<F>>
         DataType.Partial<Either5<A, B, C, D, E>, Tuple5<A, DA, B, DB, C, DC, D, DD, E, DE>>.plus(sixth: DF): DataType.Partial<Either6<A, B, C, D, E, F>, Tuple6<A, DA, B, DB, C, DC, D, DD, E, DE, F, DF>> =
         either6("first", schema.First.type, "second", schema.Second.type, "third", schema.Third.type, "fourth", schema.Fourth.type, "fifth", schema.Fifth.type, "sixth", sixth)
@@ -132,6 +137,7 @@ fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : Data
                 fifthName, fifthType, sixthName, sixthType, seventhName, seventhType
         ))
 
+@JvmSynthetic // useless for Java
 @JvmName("e6plus") operator fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : DataType<D>, E, DE : DataType<E>, F, DF : DataType<F>, G, DG : DataType<G>>
         DataType.Partial<Either6<A, B, C, D, E, F>, Tuple6<A, DA, B, DB, C, DC, D, DD, E, DE, F, DF>>.plus(seventh: DG): DataType.Partial<Either7<A, B, C, D, E, F, G>, Tuple7<A, DA, B, DB, C, DC, D, DD, E, DE, F, DF, G, DG>> =
         either7("first", schema.First.type, "second", schema.Second.type, "third", schema.Third.type, "fourth", schema.Fourth.type, "fifth", schema.Fifth.type, "sixth", schema.Sixth.type, "seventh", seventh)
@@ -152,6 +158,7 @@ fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : Data
                 fifthName, fifthType, sixthName, sixthType, seventhName, seventhType, eighthName, eighthType
         ))
 
+@JvmSynthetic // useless for Java
 @JvmName("e7plus") operator fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : DataType<D>, E, DE : DataType<E>, F, DF : DataType<F>, G, DG : DataType<G>, H, DH : DataType<H>>
         DataType.Partial<Either7<A, B, C, D, E, F, G>, Tuple7<A, DA, B, DB, C, DC, D, DD, E, DE, F, DF, G, DG>>.plus(eighth: DH): DataType.Partial<Either8<A, B, C, D, E, F, G, H>, Tuple8<A, DA, B, DB, C, DC, D, DD, E, DE, F, DF, G, DG, H, DH>> =
         either8("first", schema.First.type, "second", schema.Second.type, "third", schema.Third.type, "fourth", schema.Fourth.type, "fifth", schema.Fifth.type, "sixth", schema.Sixth.type, "seventh", schema.Seventh.type, "eighth", eighth)
@@ -161,6 +168,7 @@ fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : Data
  * A placeholder to surprise less.
  * Despite this declaration is deprecated, it is not going to be removed.
  */
+@JvmSynthetic // useless for Java
 @JvmName("e8plus") @Deprecated("Either9+ are not implemented", level = DeprecationLevel.ERROR) operator fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : DataType<D>, E, DE : DataType<E>, F, DF : DataType<F>, G, DG : DataType<G>, H, DH : DataType<H>, I, DI : DataType<I>>
         DataType.Partial<Either8<A, B, C, D, E, F, G, H>, Tuple8<A, DA, B, DB, C, DC, D, DD, E, DE, F, DF, G, DG, H, DH>>.plus(ninth: DI): Nothing =
         throw UnsupportedOperationException()
@@ -169,6 +177,7 @@ fun <A, DA : DataType<A>, B, DB : DataType<B>, C, DC : DataType<C>, D, DD : Data
  * Maintains addition associativity.
  * Despite this declaration is deprecated, it is not going to be removed.
  */
+@JvmSynthetic // useless for Java
 @JvmName("plusE") @Deprecated("right operand is not expected to be Either", level = DeprecationLevel.ERROR) operator fun
         DataType.Partial<out Either8<*, *, *, *, *, *, *, *>, out Tuple8<*, *, *, *, *, *, *, *, *, *, *, *, *, *, *, *>>
         .plus(other: DataType.Partial<out Either8<*, *, *, *, *, *, *, *>, out Tuple8<*, *, *, *, *, *, *, *, *, *, *, *, *, *, *, *>>): Nothing =
