b52566a7f191aecf59f98e217848113663b033ac
Miha_x64 <mihlo1996@rambler.ru>
2020-04-12 20:47:20 +0300

    deprecated Either cases in favor of .map and .fold && mv either.kt eitherN.kt

--- extended-persistence/src/main/kotlin/net/aquadc/persistence/extended/either/eitherN.kt
+++ extended-persistence/src/main/kotlin/net/aquadc/persistence/extended/either/eitherN.kt
@@ -23,27 +23,35 @@ sealed class Either8<out A, out B, out C, out D, out E, out F, out G, out H>(
         // with 4-byte classword, 4-byte OOPS and 8-byte padding `which` field won't increase instance size
         // which will be 16 bytes
 ) {
+    @Deprecated("will be reworked without sealed classes, use .map and .fold")
     class First<out A>(value: A) : Either8<A, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing>(value, 0) {
         val value: A get() = _value as A
     }
+    @Deprecated("will be reworked without sealed classes, use .map and .fold")
     class Second<out B>(value: B) : Either8<Nothing, B, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing>(value, 1) {
         val value: B get() = _value as B
     }
+    @Deprecated("will be reworked without sealed classes, use .map and .fold")
     class Third<out C>(value: C) : Either8<Nothing, Nothing, C, Nothing, Nothing, Nothing, Nothing, Nothing>(value, 2) {
         val value: C get() = _value as C
     }
+    @Deprecated("will be reworked without sealed classes, use .map and .fold")
     class Fourth<out D>(value: D) : Either8<Nothing, Nothing, Nothing, D, Nothing, Nothing, Nothing, Nothing>(value, 3) {
         val value: D get() = _value as D
     }
+    @Deprecated("will be reworked without sealed classes, use .map and .fold")
     class Fifth<out E>(value: E) : Either8<Nothing, Nothing, Nothing, Nothing, E, Nothing, Nothing, Nothing>(value, 4) {
         val value: E get() = _value as E
     }
+    @Deprecated("will be reworked without sealed classes, use .map and .fold")
     class Sixth<out F>(value: F) : Either8<Nothing, Nothing, Nothing, Nothing, Nothing, F, Nothing, Nothing>(value, 5) {
         val value: F get() = _value as F
     }
+    @Deprecated("will be reworked without sealed classes, use .map and .fold")
     class Seventh<out G>(value: G) : Either8<Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, G, Nothing>(value, 6) {
         val value: G get() = _value as G
     }
+    @Deprecated("will be reworked without sealed classes, use .map and .fold")
     class Eighth<out H>(value: H) : Either8<Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, Nothing, H>(value, 7) {
         val value: H get() = _value as H
     }
@@ -62,11 +70,26 @@ sealed class Either8<out A, out B, out C, out D, out E, out F, out G, out H>(
 }
 
 // nested types are not visible through typealiases
+@Deprecated("will be reworked without sealed classes, use .map and .fold")
 typealias EitherLeft<A> = Either8.First<A>
+
+@Deprecated("will be reworked without sealed classes, use .map and .fold")
 typealias EitherRight<B> = Either8.Second<B>
+
+@Deprecated("will be reworked without sealed classes, use .map and .fold")
 typealias EitherThird<C> = Either8.Third<C>
+
+@Deprecated("will be reworked without sealed classes, use .map and .fold")
 typealias EitherFourth<D> = Either8.Fourth<D>
+
+@Deprecated("will be reworked without sealed classes, use .map and .fold")
 typealias EitherFifth<E> = Either8.Fifth<E>
+
+@Deprecated("will be reworked without sealed classes, use .map and .fold")
 typealias EitherSixth<F> = Either8.Sixth<F>
+
+@Deprecated("will be reworked without sealed classes, use .map and .fold")
 typealias EitherSeventh<G> = Either8.Seventh<G>
+
+@Deprecated("will be reworked without sealed classes, use .map and .fold")
 typealias EitherEighth<H> = Either8.Eighth<H>
