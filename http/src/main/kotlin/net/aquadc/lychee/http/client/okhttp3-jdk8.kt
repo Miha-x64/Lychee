49e25b9df02090103dd46af0a15b91088c647533
Miha_x64 <mihlo1996@rambler.ru>
2020-06-06 21:56:04 +0300

    http: CompletionStage adapter for OkHttp

---
+++ http/src/main/kotlin/net/aquadc/lychee/http/client/okhttp3-jdk8.kt
@@ -0,0 +1,33 @@
+@file:JvmName("OkHttp3Jdk8")
+package net.aquadc.lychee.http.client
+
+import net.aquadc.lychee.http.param.Resp
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionStage
+
+
+fun <T> completable(
+    parse: (Resp<T>, Response) -> T
+): (OkHttpClient, Request, Resp<T>) -> CompletionStage<T> =
+    { client, request, body ->
+        val future = CompletableFuture<T>()
+        val call = client.newCall(request)
+        call.enqueue(object : Callback {
+            override fun onResponse(call: Call, response: Response) {
+                future.complete(parse(body, response))
+            }
+            override fun onFailure(call: Call, e: IOException) {
+                future.completeExceptionally(e)
+            }
+        })
+        future.whenComplete { _, _ ->
+            if (future.isCancelled) call.cancel()
+        }
+        future
+    }
