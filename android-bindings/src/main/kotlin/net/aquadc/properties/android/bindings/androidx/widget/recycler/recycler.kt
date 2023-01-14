dd7e89bda5482c23dafe28f2c026abc2e153a3ee
Miha-x64 <mihlo1996@rambler.ru>
2023-01-14 11:16:58 +0300

    allow multiple RV.observeAdapter() calls safely

--- android-bindings/src/main/kotlin/net/aquadc/properties/android/bindings/androidx/widget/recycler/recycler.kt
+++ android-bindings/src/main/kotlin/net/aquadc/properties/android/bindings/androidx/widget/recycler/recycler.kt
@@ -3,6 +3,7 @@ package net.aquadc.properties.android.bindings.androidx.widget.recycler
 
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import net.aquadc.properties.android.R
 
 /**
  * Calls [RecyclerView.swapAdapter] (adapter, false) respecting attached state:
@@ -11,16 +12,30 @@ import androidx.recyclerview.widget.RecyclerView
  * this may help unobserving properties (but not unreferencing them).
  * @see ObservingAdapter
  */
-fun RecyclerView.observeAdapter(adapter: RecyclerView.Adapter<*>) {
-    addOnAttachStateChangeListener(object : View.OnAttachStateChangeListener {
-        override fun onViewAttachedToWindow(p0: View?) {
-            (p0 as RecyclerView).swapAdapter(adapter, false)
+fun RecyclerView.observeAdapter(adapter: RecyclerView.Adapter<*>?) {
+    val attacher = getTag(R.id._lychee_adapterAttacher)
+    if (attacher == null)
+        AdapterAttacher(adapter).also {
+            setTag(R.id._lychee_adapterAttacher, it)
+            addOnAttachStateChangeListener(it)
         }
-        override fun onViewDetachedFromWindow(p0: View?) {
-            (p0 as RecyclerView).swapAdapter(null, false)
-        }
-    })
+    else
+        (attacher as AdapterAttacher).adapter = adapter
 
     if (isAttachedToWindow /*this is overridden in RecyclerView, no minSdk problem*/)
         swapAdapter(adapter, false)
+}
+
+val RecyclerView.observedAdapter: RecyclerView.Adapter<*>?
+    get() = (getTag(R.id._lychee_adapterAttacher) as AdapterAttacher?)?.adapter
+
+private class AdapterAttacher(
+    @JvmField var adapter: RecyclerView.Adapter<*>?,
+) : View.OnAttachStateChangeListener {
+    override fun onViewAttachedToWindow(p0: View?) {
+        (p0 as RecyclerView).swapAdapter(adapter, false)
+    }
+    override fun onViewDetachedFromWindow(p0: View?) {
+        (p0 as RecyclerView).swapAdapter(null, false)
+    }
 }
