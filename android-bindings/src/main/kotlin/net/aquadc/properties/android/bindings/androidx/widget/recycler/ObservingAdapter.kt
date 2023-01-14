b95c85f53039a09fc91ef74131c0c71f0fa24d6b
Miha-x64 <mihlo1996@rambler.ru>
2023-01-14 11:17:58 +0300

    added ObservingListAdapter(Property<List>, DiffCallback | DifferConfig)

--- android-bindings/src/main/kotlin/net/aquadc/properties/android/bindings/androidx/widget/recycler/ObservingAdapter.kt
+++ android-bindings/src/main/kotlin/net/aquadc/properties/android/bindings/androidx/widget/recycler/ObservingAdapter.kt
@@ -1,7 +1,13 @@
 package net.aquadc.properties.android.bindings.androidx.widget.recycler
 
 import androidx.annotation.CallSuper
+import androidx.recyclerview.widget.AsyncDifferConfig
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import net.aquadc.properties.ChangeListener
+import net.aquadc.properties.Property
+import net.aquadc.properties.syncIf
 
 /**
  * A simple abstract [RecyclerView.Adapter] which knows whether it is being observed by [RecyclerView] instances.
@@ -29,4 +35,31 @@ abstract class ObservingAdapter<VH : RecyclerView.ViewHolder> : RecyclerView.Ada
      */
     protected abstract fun onObservedStateChanged(observed: Boolean)
 
+}
+
+abstract class ObservingListAdapter<T, VH : RecyclerView.ViewHolder> : ListAdapter<T, VH> {
+
+    private val dataProperty: Property<List<T>>
+    constructor(dataProperty: Property<List<T>>, diffCallback: DiffUtil.ItemCallback<T>) : super(diffCallback) {
+        this.dataProperty = dataProperty
+    }
+    constructor(dataProperty: Property<List<T>>, config: AsyncDifferConfig<T>) : super(config) {
+        this.dataProperty = dataProperty
+    }
+
+    private var recyclers = 0
+    @CallSuper override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        if (recyclers++ == 0) onObservedStateChanged(true)
+    }
+    @CallSuper override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
+        if (--recyclers == 0) onObservedStateChanged(false)
+        super.onDetachedFromRecyclerView(recyclerView)
+    }
+    @CallSuper protected fun onObservedStateChanged(observed: Boolean) {
+        dataProperty.syncIf(observed, changeListener, dummy = emptyList())
+    }
+    private val changeListener: ChangeListener<List<T>> = { _, new ->
+        submitList(new)
+    }
 }
